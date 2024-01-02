41f0b2c2c023beff3f4636166a840d3aa21221d1
Hemanthkumar <107429016+hacker-phk@users.noreply.github.com>
2024-01-02 10:52:59 +0530

    Update index4.js
    this is about when we request file and the  response send the file

--- index4.js
+++ index4.js
@@ -1,8 +1,25 @@
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 import express from "express";
 
 const app = express();
-const port = 3000;
-
+const port = 5000;
+app.use(bodyParser.urlencoded({extended: true}));
+app.get('/',(req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+var name="";
+const merabody=(req,res,next) => {
+  console.log(req.body);
+  name = req.body.street;
+  next();
+};
+app.use(merabody);
+app.post('/submit',(req,res) => {
+res.send(`<p>${name}</p>`)
+});
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
